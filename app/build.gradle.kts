ad2898fbd7a80a4c90c7326c58b5221d33880854
andersbor <anbo@zealand.dk>
2025-03-30 11:53:39 +0200

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.implicitintentexample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.implicitintentexample"
